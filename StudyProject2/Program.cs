097ad495bb6e0c9d97095b70ba4063018611f186
kirillkhlopkoff <114176969+kirillkhlopkoff@users.noreply.github.com>
2023-04-07 18:38:36 +0300

    services

--- StudyProject2/Program.cs
+++ StudyProject2/Program.cs
@@ -1,10 +1,28 @@
-using StudyApp2;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder();
+
+var services = builder.Services;
+
 var app = builder.Build();
 
-app.UseMiddleware<ErrorHandlingMiddleware>();
-app.UseMiddleware<AuthenticationMiddleware>();
-app.UseMiddleware<RoutingMiddleware>();
+app.Run(async context =>
+{
+    var sb = new StringBuilder();
+    sb.Append("<h1>Все сервисы</h1>");
+    sb.Append("<table>");
+    sb.Append("<tr><th>Тип</th><th>Lifetime</th><th>Реализация</th></tr>");
+    foreach (var svc in services)
+    {
+        sb.Append("<tr>");
+        sb.Append($"<td>{svc.ServiceType.FullName}</td>");
+        sb.Append($"<td>{svc.Lifetime}</td>");
+        sb.Append($"<td>{svc.ImplementationType?.FullName}</td>");
+        sb.Append("</tr>");
+    }
+    sb.Append("</table>");
+    context.Response.ContentType = "text/html;charset=utf-8";
+    await context.Response.WriteAsync(sb.ToString());
+});
 
 app.Run();
